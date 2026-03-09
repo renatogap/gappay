924196a479b72b321039134ac2d7f85dba35202b
Renato Pereira <renato.19gp@gmail.com>
2026-03-09 11:27:55 -0300

    Ajuste de erros

--- resources/views/pedido/pedidos-pendentes-gerente.blade.php
+++ resources/views/pedido/pedidos-pendentes-gerente.blade.php
@@ -16,13 +16,13 @@
     @if (session('sucesso'))
     <div class="alert alert-success">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {!! session('sucesso') !!}
+        {{ session('sucesso') }}
     </div>
     @endif
     @if (session('error'))
     <div class="alert alert-danger">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        {!! session('error') !!}
+        {{ session('error') }}
     </div>
     @endif
 
